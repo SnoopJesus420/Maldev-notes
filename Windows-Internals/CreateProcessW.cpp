f4f92f781cca1beca17ac3cdfa5906af9dc4128d
John Doe <51519499+SnoopJesus420@users.noreply.github.com>
2024-07-29 16:19:25 -0700

    Create CreateProcessW.cpp
    Simple CPP program to start notepad.exe via CreateProcessW API call, and returns an error via GetLastError() API call.

---
+++ Windows-Internals/CreateProcessW.cpp
@@ -0,0 +1,40 @@
+#include <Windows.h>
+#include<iostream>
+using namespace std;
+
+int main(void) {
+
+	// Initialize STARTUPINFOW struct
+	STARTUPINFOW si;
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+
+
+	// Initialize PROCESS_INFORMATION struct
+	PROCESS_INFORMATION pi;
+	ZeroMemory(&pi, sizeof(pi));
+
+	// Variable to store command
+	LPCWSTR cmd = L"notepad.exe";
+
+
+	// Creating the process
+	if (CreateProcessW(NULL, const_cast<LPWSTR>(cmd), NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+
+		// Wait until child process exits
+		WaitForSingleObject(pi.hProcess, INFINITE);
+
+		// Close the process and threads
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+
+	}
+	else {
+
+		// Display an error message via GetLastError() API call
+		std::cerr << "CreateProcessW failed (" << GetLastError() << ").\n";
+	}
+
+	return 0;
+
+}
